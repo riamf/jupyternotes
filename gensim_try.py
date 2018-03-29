e2aa7fb8af05f088f09b397991e33e65aba6e6d6
Pawel Kowalczuk <pawelkowalczuk@230415071.local>
2018-03-29 20:14:27 +0200

    cleared data and gensim working with tfidf

--- gensim_try.py
+++ gensim_try.py
@@ -2,41 +2,34 @@ from collections import defaultdict
 from gensim import corpora
 from nltk.corpus import stopwords
 from nltk.corpus import RegexpTokenizer
+from nltk.tokenize import word_tokenize
 from pprint import pprint
 from gensim.corpora import MmCorpus
 
-f = open('data.txt', 'r')
+f = open('data2.txt', 'r')
 documents = f.readlines()
 f.close()
 
-
-def preprocess(sentence):
-	sentence = sentence.lower()
-	tokenizer = RegexpTokenizer(r'\w+')
-	tokens = tokenizer.tokenize(sentence)
-	filtered_words = [w for w in tokens if not w in stopwords.words('english')]
-	return " ".join(filtered_words)
-
-texts = list(map(preprocess,documents))
+texts = list(map(word_tokenize, documents))
 pprint(texts[:3])
 
 #remove words that appear only once
 frequency = defaultdict(int)
 for text in texts:
-    for word in text.split():
+    for word in text:
         frequency[word] += 1
 
-texts = [[word for word in text.split() if frequency[word] > 1] for text in texts]
+texts = [[word for word in text if frequency[word] > 1] for text in texts]
 pprint(texts[:3])
 
 # bag of words // word: (how many times it occures)
 dictionary = corpora.Dictionary(texts)
-dictionary.save('./gen_sim.dict')
+# dictionary.save('./gen_sim.dict')
 
 # tokenize to vectors
 corpus = [dictionary.doc2bow(text) for text in texts]
 pprint(corpus[:3])
-MmCorpus.serialize('./gen_sim_corpus.mm', corpus)
+# MmCorpus.serialize('./gen_sim_corpus.mm', corpus)
 
 
 from gensim.models import TfidfModel
@@ -49,18 +42,18 @@ corpus_tfidf = tfidf[corpus]
 # for doc in corpus_tfidf:
 #     print(doc)
 
-lsi = LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2)
+lsi = LsiModel(corpus_tfidf, id2word=dictionary)
 corpus_lsi = lsi[corpus_tfidf]
 print(lsi.print_topics(2))
 
 # for doc in corpus_lsi:
 #     print(doc)
 
-lsi.save('gensim_lsi_model.lsi')
+# lsi.save('gensim_lsi_model.lsi')
 
 # transform corpus to LSI space and index it
-index = MatrixSimilarity(lsi[corpus])
-index.save('./gensim_indexes.index')
+index = MatrixSimilarity(lsi[corpus_tfidf])
+# index.save('./gensim_indexes.index')
 
 query = 'What worries me about AI'
 query_vec = dictionary.doc2bow(query.lower().split())
@@ -70,12 +63,17 @@ print(vec_lsi)
 
 # perform a similarity query against the corpus
 sims = index[vec_lsi]
-sims = sorted(enumerate(sims), key=lambda item: -item[1])
+sims_s = sorted(list(enumerate(sims)), key=lambda tup: tup[1], reverse=True)
 # sorted (document number, similarity score) 2-tuples
 print('\n')
-for i in range(0,10):
-    sim = sims[i]
-    print('{} - {}'.format(sim[1], documents[sim[0]]))
+print('Printing first 10')
+c = 0
+for item in sims_s:
+	i = item[0]
+	v = item[1]
+	c+=1
+	if c==10: break
+	print("{} - {}".format(v,documents[i]))
 
 
 
